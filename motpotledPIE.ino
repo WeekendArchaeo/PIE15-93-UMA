c5664244a09535f60db36a70a49cdf40eeecedc4
Ana Cruz Martín <anacruzmartin@gmail.com>
2017-03-21 13:44:21 +0100

    Add files via upload
    MotPotLedPIE enciende los ledes pares de UMA Shield a diferentes frecuencias, dependiendo del valor leído del potenciómetro de UMA Shield. A partir de esa lectura, mueve el servo a diferentes posiciones.
    Este ejemplo de código es de dominio público.
    Basado en los ejemplos Blink y Sweep incluidos en el IDE de Arduino
      
    
    
    MotPotLedPIE turns on the even LEDS of the UMA Shield at different frequencies,  depending on the value read from the UMA Shield potentiometer (trimmer). Then it moves the servo to different positions according to the trimmer value read.
    This example code is in the public domain.
    Based on the Blink and the Sweep examples included in the Arduino IDE

---
+++ motpotledPIE.ino
@@ -0,0 +1,93 @@
+/*
+  MotPotLedPIE
+  Turns on the even LEDS of the UMA Shield at different frequencies,
+  depending on the value read from the UMA Shield potentiometer (trimmer).
+  Then moves the servo to different positions according to the trimmer
+  value read.
+
+  This example code is in the public domain.
+
+  Based on the Blink and the Sweep examples included in the Arduino IDE
+  
+  Ana Cruz-Martin
+  March 2017
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+int trimmer = A5;
+int trimmerRead = 0;
+
+// mapping the Arduino digital pins to the UMA Shield LEDs
+int LEDe1 = 10;
+int LEDe2 = 8;
+int LEDe3 = 5;
+int LEDe4 = A4;
+
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  // initialize digital pins as outputs
+  pinMode(LEDe1, OUTPUT);
+  pinMode(LEDe2, OUTPUT);
+  pinMode(LEDe3, OUTPUT);
+  pinMode(LEDe4, OUTPUT);
+}
+
+
+void blinkLED(int led, int timedelay)
+{
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(timedelay);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(timedelay); 
+}
+
+
+void loop() {
+  trimmerRead = analogRead(trimmer);
+  myservo.write(0);
+//  if (trimmerRead < 256)
+//  {
+//    blinkLED(LEDe1,1000); // wait for a second
+//    for (pos = 0; pos <= 45; pos += 1)  // goes from 0 degrees to 45 degrees in steps of 1 degree
+//    { 
+//       myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//       delay(15);                       // waits 15ms for the servo to reach the position
+//    }
+//  }
+//  else if ((trimmerRead >= 256) & (trimmerRead < 512))
+//  {
+//    blinkLED(LEDe2,750);  // wait for 750 milliseconds
+//    for (pos = 45; pos <= 90; pos += 1) // goes from 45 degrees to 90 degrees in steps of 1 degree
+//    {
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//      delay(15);                       // waits 15ms for the servo to reach the position
+//    }
+//  }
+//  else if ((trimmerRead >= 512) & (trimmerRead < 768))
+//  {
+//    blinkLED(LEDe3,500);  // wait for 500 milliseconds
+//    for (pos = 90; pos <= 135; pos += 1) // goes from 90 degrees to 135 degrees in steps of 1 degree
+//    {
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//      delay(15);                       // waits 15ms for the servo to reach the position
+//    }
+//  }
+//  else
+//  {
+//   blinkLED(LEDe4,250); // wait for 250 milliseconds
+//   for (pos = 120; pos <= 180; pos += 1) // goes from 120 degrees to 180 degrees in steps of 1 degree
+//   { 
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//      delay(15);                       // waits 15ms for the servo to reach the position
+//   }
+//  }
+  delay(100);
+}
